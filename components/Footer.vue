a30e6a7d2de4b13b20efa7feebbd9e48f975410c
lilong <lilong@gmail.com>
2018-07-03 17:52:32 +0900

    初始nuxt模版

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    Bookmark our website for more information : <a href="https://mydapp.io">mydapp.io</a>
+  </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <img src="../assets/img/logo.png" alt="mydapp.io" class="logo" />
+    <h1 class="title">
+      Decentralized Web Hosting for Decentralized Applications
+    </h1>
+    <!--<nuxt-link class="button" to="/about">-->
+      <!--About page-->
+    <!--</nuxt-link>-->
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
